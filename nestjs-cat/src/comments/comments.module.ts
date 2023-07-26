4335dfaeaccc1e6a4e5180ecb3d6d323b9e17323
dev_sm <tmdals9974@naver.com>
2023-07-26 23:02:02 +0900

    fix: cat project 실행 오류 해결

--- nestjs-cat/src/comments/comments.module.ts
+++ nestjs-cat/src/comments/comments.module.ts
@@ -3,12 +3,14 @@ import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Comments, CommentsSchema } from './comments.schema';
+import { CatsModule } from 'src/cats/cats.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Comments.name, schema: CommentsSchema },
     ]),
+    CatsModule,
   ],
   controllers: [CommentsController],
   providers: [CommentsService],
